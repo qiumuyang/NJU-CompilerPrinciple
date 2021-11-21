911c843462e2d297852f723f3dfe555bb9dffce8
QMY <782719906@qq.com>
2021-11-21 23:13:12 +0800

    fix: update irsim cli

--- irsim/irsim_cli.py
+++ irsim/irsim_cli.py
@@ -1,9 +1,13 @@
+import argparse
 import sys
 import os
+import time
 import platform
 from typing import Dict, List, Tuple
 
 __version__ = '1.0.2'
+
+MAX_TIME_OUT = 10
 
 
 def error(*arg):
@@ -31,24 +35,32 @@ class CurrentFunctionNoneError(Exception):
     pass
 
 
+class UnknownOpCodeError(Exception):
+    pass
+
+
 class IRSimCli():
 
-    def __init__(self, inputFile: str, stdin: List[int]) -> None:
+    def __init__(self, inputFile: str, stdin: List[int], divC: bool = False) -> None:
         self.ip: int = -1
         self.entranceIP: int = -1
         self.offset: int = 0
         self.instrCnt: int = 0
         self.codes: List[tuple] = list()
         self.rawcodes: List[str] = list()
-        self.mem: List[int] = None
+        self.mem: List[int] = list()
         self.functionDict: Dict[str, List[str]] = dict()
         self.currentFunction = None
-        self.symTable: Dict[str, List[Tuple[int, int, bool]]] = dict()
+        self.symTable: Dict[str, Tuple[int, int, bool]] = dict()
         self.labelTable = dict()
         self.callStack = list()
         self.argumentStack = list()
         self.input = stdin
         self.loadFile(inputFile)
+        self.console = []
+        self.start_time = time.time()
+        self.write_time = 0
+        self.divC = divC
 
     def loadFile(self, fname: str):
         if not os.path.exists(fname):
@@ -81,6 +93,10 @@ class IRSimCli():
                 break
             code = self.codes[self.ip]
             error_code = self.executeCode(code)
+
+            if time.time() - self.start_time > MAX_TIME_OUT:
+                error_code = 4
+
             if error_code > 0:
                 break
             self.ip += 1
@@ -91,12 +107,16 @@ class IRSimCli():
                     f"Program exited with input remaining:", *self.input)
             print(
                 f'Program has exited gracefully.\nTotal instructions = {self.instrCnt}')
+            print(f'Output: {" ".join(self.console)}')
             exit(0)
         elif error_code == 2:
             error('An error occurred at line %d: Illegal memory access. \nIf this message keeps popping out, please reload the source file' % (self.ip + 1))
         elif error_code == 3:
             error(
                 'Program Counter goes out of bound. The running program will be terminated instantly.')
+        elif error_code == 4:
+            error(
+                f'Program runs for more than {MAX_TIME_OUT} seconds. Check dead loop or make "MAX_TIME_OUT" larger.')
         self.ip = -1
 
     def stop(self):
@@ -105,6 +125,8 @@ class IRSimCli():
         self.mem = [0] * 262144
         self.callStack = list()
         self.argumentStack = list()
+        self.start_time = time.time()
+        self.write_time = 0
 
     def sanityCheck(self, code: str, lineno: int) -> None:
         strs = code.split()
@@ -145,7 +167,7 @@ class IRSimCli():
                     if strs[1] in self.symTable:
                         raise DuplicatedVariableError
                     self.tableInsert(strs[1], int(strs[2]), True)
-                    self.codes.append('DEC')
+                    self.codes.append(('DEC',))
                 elif strs[0] == 'IF':
                     if len(strs) != 6 or strs[4] != 'GOTO' or strs[2] not in relops:
                         raise IRSyntaxError
@@ -238,7 +260,8 @@ class IRSimCli():
                 else:
                     error("Input exhausted but still asked for another input")
             elif code[0] == 'WRITE':
-                self.console.append(str(self.getValue(code[1])))
+                self.write_time += 1
+                self.console.append(str(int(self.getValue(code[1]))))
             elif code[0] == 'GOTO':
                 self.ip = self.labelTable[code[1]]
             elif code[0] == 'IF':
@@ -256,8 +279,27 @@ class IRSimCli():
             elif code[0] == 'ARITH':
                 value1 = self.getValue(code[2])
                 value2 = self.getValue(code[4])
+                op = code[3] if code[3] != '/' else '//'
+                if self.divC:
+                    sign = 1
+                    if op == '//':
+                        if value1 < 0:
+                            value1 = -value1
+                            sign = -sign
+                        if value2 < 0:
+                            value2 = -value2
+                            sign = -sign
+                    value = eval(str(value1) + op + str(value2))
+                    value *= sign
+                else:
+                    try:
+                        value = eval(str(value1) + op + str(value2))
+                    except:
+                        # print(*code)
+                        value = 0
+
                 self.mem[self.symTable[code[1]][0] //
-                         4] = eval(str(value1) + code[3] + str(value2))
+                         4] = value
             elif code[0] == 'RETURN':
                 if len(self.callStack) == 0:
                     return 1
@@ -284,6 +326,10 @@ class IRSimCli():
             elif code[0] == 'PARAM':
                 self.mem[self.symTable[code[1]][0] //
                          4] = self.argumentStack.pop()
+            elif code[0] in ['DEC', 'LABEL']:
+                pass
+            else:
+                error(f'Unknown operation code {code[0]}.')
         except IndexError:
             return 2
 
@@ -296,12 +342,13 @@ class IRSimCli():
 
 
 if __name__ == '__main__':
-    if len(sys.argv) == 1:
-        error('Usage: python3 irsim_cli.py <input.ir> [<input integer> ...]')
-    filename = sys.argv[1]
-    for arg in sys.argv[2:]:
-        if not arg.isdigit():
-            error(f'Input should be an integer, {arg} found')
-    inputList = [int(arg) for arg in sys.argv[2:]]
-    irsim = IRSimCli(filename, inputList)
-    irsim.run()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input', type=str,
+                        help='input program file')
+    parser.add_argument('number', type=int, nargs='*',
+                        help='input for the program')
+    parser.add_argument('-c', '--cdiv', action="store_true",
+                        help='divide like c or not')
+    args = parser.parse_args(sys.argv[1:])
+
+    IRSimCli(args.input, args.number, args.cdiv).run()
